2655c61f8e77f00c6fcac01c964154e3b4197e88
Alexandre Bouça <epicgamer0020@gmail.com>
2021-07-08 14:38:50 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Error.wtf</h1>
+        <p className="Sub-header">Under construction</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

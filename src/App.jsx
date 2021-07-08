ad523879de22ab4d6e6ffcef8f489c3c96cb5541
Alexandre Bouça <epicgamer0020@gmail.com>
2021-07-08 18:59:16 +0100

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,32 @@ function App() {
       <header className="App-header">
         <h1>Error.wtf</h1>
         <p className="Sub-header">Under construction</p>
+        <div className="coins">
+          <div className="coin">
+            <img
+              src="https://cdn.worldvectorlogo.com/logos/bitcoin.svg"
+              alt="BTC"
+              className="coinicon"
+            />
+            <p>17GhizAzgcA6oshvUvFpJ6hWsoQZXptTNW</p>
+          </div>
+          <div className="coin">
+            <img
+              src="https://cdn.worldvectorlogo.com/logos/ethereum-eth.svg"
+              alt="ETH"
+              className="coinicon"
+            />
+            <p>10x9becb6e0793a7597f6f0a84190e30313450468bd</p>
+          </div>
+          <div className="coin">
+            <img
+              src="https://cdn.worldvectorlogo.com/logos/dogecoin-1.svg"
+              alt="DOGE"
+              className="coinicon"
+            />
+            <p>DStA7SZqPEub5HJcbjJALEEZJA9tRnrxuP</p>
+          </div>
+        </div>
       </header>
     </div>
   );
